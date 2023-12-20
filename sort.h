b0c40fe8d78fd3ef5226b042a50b34233f8eae2e
aminekell <aminekella2003.com>
2023-12-20 08:00:27 -0800

    done

--- sort.h
+++ sort.h
@@ -17,6 +17,7 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 /* Other function prototypes... */
 
 #endif /* SORT_H */
